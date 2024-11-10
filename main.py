8afbfc731490e9f48f68eef7541373a2618b27aa
phuongman <22021119@vnu.edu.vn>
2024-11-10 17:07:39 +0700

    Merge code

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import cv2
 from Clustering_filter import Clustering_Filter 
+import prepare_lowpass_downsampled_input as pre
+from Interpolation_filter import EdgeSensitiveInterpolator
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    image = plt.imread("aa.webp")
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    height, width = gray_image.shape
-    pre_image = Clustering_Filter(gray_image, height, width, 0.5, 5, 40, 0.5)
-    pre_image.process_image()
+    path = "lenna.png"
+    original_image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    #original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+
+    degraded_image = pre.lowpass_and_downsample(original_image, sigma=1.0, factor=2)
+
+    #Interpolation_filter
+    interpolator = EdgeSensitiveInterpolator(k = 1.0)
+    proposed_image = interpolator.proposed_interpolation(degraded_image)
+
+
+    height, width = proposed_image.shape
+    reconstructed_image = Clustering_Filter(proposed_image, height, width, 0.5, 5, 40, 0.5)
+    reconstructed_image.process_image()
     
